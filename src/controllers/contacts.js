3a7e5718a0975d180be42fc20e2b595ab11352e4
Arina Kapitula <verhovskayarina@gmail.com>
2025-04-21 23:41:37 +0300

    Fix broken imports and deployment errors

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,7 +6,7 @@ import {
   getContactById,
   patchContact,
 } from '../services/contacts.js';
-import { parsePaginationParams } from '../utils/parseFilterParams.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 
