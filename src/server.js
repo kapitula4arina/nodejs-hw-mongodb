9bd92af7775ea272ba1929dbfab3ea108765feb1
Arina Kapitula <verhovskayarina@gmail.com>
2025-05-05 00:16:07 +0300

    feat: add complete API documentation using Swagger and ReDocly

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -17,6 +18,7 @@ export const setupServer = () => {
   app.use(cors());
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use(cookieParser());
+
   app.use(pino({ transport: { target: 'pino-pretty' } }));
   app.use(
     express.json({
@@ -24,6 +26,8 @@ export const setupServer = () => {
       limit: '100kb',
     }),
   );
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
